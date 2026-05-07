d46da715575ac0441e1013c57bfe4605c195e7c6
Yehyaismail <145143935+Yehyaismail@users.noreply.github.com>
2026-05-07 15:09:54 +0300

    Update AuthContext.js

--- frontend/src/contexts/AuthContext.js
+++ frontend/src/contexts/AuthContext.js
@@ -18,6 +18,7 @@ export const AuthProvider = ({ children }) => {
 
   
 const API_URL = process.env.REACT_APP_API_URL;
+// eslint-disable-next-line react-hooks/exhaustive-deps
 
   useEffect(() => {
     checkAuth();
